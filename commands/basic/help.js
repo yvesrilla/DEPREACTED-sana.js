b36598ae7f4d61311dec331c5a76d5cd6e48c1a7
mich <76213511+yvesrilla@users.noreply.github.com>
2021-07-28 16:25:50 +0100

    updated help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -1,4 +1,27 @@
+const Discord = require('discord.js');
+
 module.exports={
-  name: "help",
-  aliases: ["h", "gib help"]
+    name: "help",
+    aliases: ["h", "gib help"],
+    async run(sana, message, args){
+        //embed creation
+
+        const help = new Discord.MessageEmbed()
+        .setDescription("**sana's help panel!**"
+        +"\n welcome to my help panel, here you'll see all of my available commands!"
+        +"\n"
+        +"\n __basic commands__"
+        +"\n `.h` `.about` `.support` `.status` `.premium`"
+        +"\n"
+        +"\n __levelling commands__"
+        +"\n `.rank` `.lb` `.leaderboard`"
+        +"\n"
+        +"\n __music commands__"
+        +"\n `.play` `.leave` `.queue`"
+        +"\n"
+        +"\n __trading card commands__"
+        +"\n in the **works**!")
+
+        message.lineReply({help})
+    }
 }
