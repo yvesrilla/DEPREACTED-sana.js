de2f61444b4323bbe972e99af918562e425c34b3
mich <76213511+yvesrilla@users.noreply.github.com>
2021-08-01 15:58:38 +0000

    completed about.js, changed the start command and created the license with the Mozilla License 2.0

---
+++ commands/basic/about.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+
+  name: "about",
+  aliases:["abt"],
+  async run(sana, message, args){
+
+    var about = new MessageEmbed()
+    .setDescription("**About Sana!**"
+        +"\n"
+        +"\n sana was born on the 29th of december, 1996 and she is a :capricorn:!"
+        +"\n sana (bot) was created on the 26th of july 2021."
+        +"\n sana (bot) was created by michal#1336"
+        +"\n [discord.js](discord.js.org) helps sana to connect to discord!")
+
+  }
+}
